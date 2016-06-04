59ba8da0b11ab9e626c6d2aa8d3ca99d3aadb105
Ray Ozzie <ray@ozzie.net>
2016-06-04 15:31:36 -0400

    moved to a subdir structure so golang can directly import via 'import github.com/rayozzie/teletype-proto/golang' without complaining about the presence of clang files

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 ## placed on the standard path after installing google's PB support
-protoc --go_out=. teletype.proto
+protoc --go_out=./golang teletype.proto
 
 ## Download and unzip nanopb sdk into a folder, and point to it with the env var NANOPBSDK
 ## https://koti.kapsi.fi/jpa/nanopb/
 ## for example: export NANOPBSDK=~/dev/nano/nanopb
-${NANOPBSDK}/generator-bin/protoc -I./ -I${NANOPBSDK}/generator/proto/ --nanopb_out=. teletype.proto
+${NANOPBSDK}/generator-bin/protoc -I./ -I${NANOPBSDK}/generator/proto/ --nanopb_out=./clang teletype.proto
