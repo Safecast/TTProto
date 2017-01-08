4422269e0057f9c943ef6cc7bef99286224aab42
Ray Ozzie <ray@ozzie.net>
2017-01-08 15:08:40 +0900

    m

--- make.sh
+++ make.sh
@@ -4,5 +4,5 @@ protoc --go_out=./golang teletype.proto
 ## Download and unzip nanopb sdk into a folder, and point to it with the env var NANOPBSDK
 ## https://koti.kapsi.fi/jpa/nanopb/
 ## Then,
-export NANOPBSDK=~/dev/nordic/nanopb
+export NANOPBSDK=~/dev/nordic/external/nano-pb
 ${NANOPBSDK}/generator-bin/protoc -I./ -I${NANOPBSDK}/generator/proto/ --nanopb_out=./clang teletype.proto
