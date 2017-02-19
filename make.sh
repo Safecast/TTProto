121813ad4bec1b90ff2fc04c3cb78b1438aa6394
Ray Ozzie <ray@ozzie.net>
2017-02-19 16:03:03 -0500

    m

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 ## protoc executable is placed on the standard path after installing standard PB support
-protoc --go_out=./golang teletype.proto
+protoc --go_out=./golang tt.proto
 
 ## Download and unzip nanopb sdk into a folder, and point to it with the env var NANOPBSDK
 ## https://koti.kapsi.fi/jpa/nanopb/
 ## Then,
 export NANOPBSDK=~/dev/nordic/external/nano-pb
-${NANOPBSDK}/generator-bin/protoc -I./ -I${NANOPBSDK}/generator/proto/ --nanopb_out=./clang teletype.proto
+${NANOPBSDK}/generator-bin/protoc -I./ -I${NANOPBSDK}/generator/proto/ --nanopb_out=./clang tt.proto
