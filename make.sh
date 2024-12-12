fb7784e66a8b29cb5b0cd01f1453d7c10b696932
Ray Ozzie <ray@ozzie.net>
2024-12-12 09:40:31 -0500

    update to latest proto

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 ## protoc executable is placed on the standard path after installing standard PB support
-protoc --go_out=./golang tt.proto
+##old was "protoc --go_out=./golang tt.proto"
+protoc --go_out=./golang --go_opt=Mtt.proto=. tt.proto
 
 ## Download and unzip nanopb sdk into a folder, and point to it with the env var NANOPBSDK
 ## https://koti.kapsi.fi/jpa/nanopb/
